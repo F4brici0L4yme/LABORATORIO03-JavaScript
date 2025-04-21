a4345809aea88ec831cdfc072178fc1ea8e31917
F4brici0L4yme <rlaymes@unsa.edu.pe>
2025-04-21 15:51:58 -0500

    Pagina2.html completa

---
+++ ejercicios_paginas_web/scriptPagina2.js
@@ -0,0 +1,31 @@
+function calcular() {
+    const op = document.getElementById("operator").value;
+    const a = document.getElementById("argument1").value;
+    const b = document.getElementById("argument2").value;
+    let result = "—";
+
+    if (["+", "-", "*", "/", "%"].includes(op)) {
+        const x = parseFloat(a), y = parseFloat(b);
+        if (isNaN(x) || isNaN(y)) return mostrar("Valores inválidos");
+        if ((op === "/" || op === "%") && y === 0) return mostrar("División por cero");
+        result = { "+": x + y, "-": x - y, "*": x * y, "/": x / y, "%": x % y }[op];
+    }
+
+    else if (["&&", "||"].includes(op)) {
+        const x = a === "1", y = b === "1";
+        result = { "&&": x && y, "||": x || y }[op];
+    }
+
+    else if (["&", "|"].includes(op)) {
+        const x = parseInt(a), y = parseInt(b);
+        if (isNaN(x) || isNaN(y)) return mostrar("Valores inválidos");
+        result = { "&": x & y, "|": x | y }[op].toString(2);
+    }
+
+    mostrar(result);
+}
+
+function mostrar(result) {
+    document.getElementById("result").textContent = result;
+    console.log('Resultado:' + result);
+}
