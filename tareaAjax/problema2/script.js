0d2c9645def6c1c0e10cadeeefc6231843b3f43e
F4brici0L4yme <rlaymes@unsa.edu.pe>
2025-04-23 17:26:49 -0500

    data filtrada - solo 1 update en la gen

--- tareaAjax/problema2/script.js
+++ tareaAjax/problema2/script.js
@@ -39,29 +39,26 @@ window.onload = function () {
     fetch('data.json')
         .then(response => response.json())
         .then(data => {
-            regionesData = data;
-            data.forEach(region => {
-                if(region.region !== 'Lima' && region.region !== 'Callao'){
-                    const labels = regionesData[0]?.confirmed.map(punto => punto.date) || [];
-                    const datasets = regionesData.map(region => ({
-                        label: region.region,
-                        data: region.confirmed.map(p => parseInt(p.value)),
-                        borderColor: colorAleatorio(),
-                        fill: false,
-                        tension: 0.2
-                    }));
+            regionesData = data.filter(t => t.region !== 'Lima' && t.region !== 'Callao');
+            const labels = regionesData[0]?.confirmed.map(punto => punto.date) || [];
+            const datasets = regionesData.map(region => ({
+                label: region.region,
+                data: region.confirmed.map(p => parseInt(p.value)),
+                borderColor: colorAleatorio(),
+                fill: false,
+                tension: 0.2
+            }));
             
-                    chart.data.labels = labels;
-                    chart.data.datasets = datasets;
-                    chart.update();
-                }
+                chart.data.labels = labels;
+                chart.data.datasets = datasets;
+                chart.update();
 
             });
-        });
-        function colorAleatorio() {
-            const r = Math.floor(Math.random() * 250);
-            const g = Math.floor(Math.random() * 250);
-            const b = Math.floor(Math.random() * 250);
-            return `rgb(${r},${g},${b})`;
-        }
 };
+
+function colorAleatorio() {
+    const r = Math.floor(Math.random() * 250);
+    const g = Math.floor(Math.random() * 250);
+    const b = Math.floor(Math.random() * 250);
+    return `rgb(${r},${g},${b})`;
+}
