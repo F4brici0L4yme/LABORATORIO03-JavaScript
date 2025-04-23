e990f8bfb6bbd6c633c9fe5f8afa58b0cc9d4cbe
F4brici0L4yme <rlaymes@unsa.edu.pe>
2025-04-22 19:29:07 -0500

    Pagina1.html corregida

--- laboratorio_03/ejercicios_paginas_web/scriptPagina1.js
+++ laboratorio_03/ejercicios_paginas_web/scriptPagina1.js
@@ -1,10 +1,23 @@
-function changeTextFontSize() {
+let currentFontSize = 16;
+
+function updateFontSize() {
     const text = document.getElementById("text");
-    const size = document.getElementById("size").value;
-    const sizePx = text.style.fontSize = size + "px";
-    text.style.fontSize = sizePx;
-    console.log("Tamaño de fuente cambiado a:", sizePx);
+    text.style.fontSize = currentFontSize + "px";
+    console.log("Tamaño de fuente cambiado a:", currentFontSize + "px");
 }
+
+function increaseFontSize() {
+    currentFontSize += 2;
+    updateFontSize();
+}
+
+function decreaseFontSize() {
+    if (currentFontSize > 2) {
+        currentFontSize -= 2;
+        updateFontSize();
+    }
+}
+
 function changeColor() {
     const text = document.getElementById("text");
     const colorInput = document.getElementById("colorInput");
