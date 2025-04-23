d841d0c8692fed64c9716537228b9b4421859a98
F4brici0L4yme <rlaymes@unsa.edu.pe>
2025-04-23 07:54:47 -0500

    data.json conectado con script

--- tareaAjax/script.js
+++ tareaAjax/script.js
@@ -1,18 +1,26 @@
 window.onload = function() {
-    const select = document.querySelector('#select');
-    fetch('https://restcountries.com/v3.1/all')
+    const select1 = document.querySelector('#region1');
+    const select2 = document.querySelector('#region2');
+    fetch('./data.json')
         .then(response => {
             if(!response.ok){
                 throw new Error('API error :(');
             }
-            response.json();
+            return response.json();
         })
         .then(data => {
+            data.sort((a, b) => a.name.common.localeCompare(b.name.common));
+
             data.forEach(country =>{
-                const option = document.createElement('option');
-                option.value = country.name.common;
-                option.textContent = country.name.common;
-                select.appendChild(option);
+                const option1 = document.createElement('option');
+                option1.value = country.name.common;
+                option1.textContent = country.name.common;
+                select1.appendChild(option1);
+            
+                const option2 = document.createElement('option');
+                option2.value = country.name.common;
+                option2.textContent = country.name.common;
+                select2.appendChild(option2);
             });
         })
         .catch(error => {
