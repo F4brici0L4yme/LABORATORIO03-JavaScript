63eaf56c088ff1d9d532da5ce66b2f98e08a9d18
F4brici0L4yme <rlaymes@unsa.edu.pe>
2025-05-03 17:23:44 -0500

    index updated - node new config - e10 completed

--- laboratorio_04/w3schools/server.js
+++ laboratorio_04/w3schools/server.js
@@ -15,6 +15,13 @@ app.use('/e6', express.static(path.join(__dirname, 'e6')));
 app.use('/e7', express.static(path.join(__dirname, 'e7')));
 app.use('/e8', express.static(path.join(__dirname, 'e8')));
 app.use('/e9', express.static(path.join(__dirname, 'e9')));
+app.get('/e10', (req, res) => {
+    res.sendFile(path.join(__dirname, 'e10/cd_catalog.xml'));
+});
+app.use('/e11', express.static(path.join(__dirname, 'e11')));
+app.use('/e12', express.static(path.join(__dirname, 'e12')));
+app.use('/e13', express.static(path.join(__dirname, 'e13')));
+app.use('/e14', express.static(path.join(__dirname, 'e14')));
 
 app.listen(3000, () => {
     console.log('Servidor corriendo en http://localhost:3000');
